93ff1b4b99e0fb83d6baace6b411794103e82037
najwanan <theunnamedrd@gmail.com>
2022-05-06 23:23:48 -0700

    trouble shooting OWL plug-in, changed order of scripts to resolve one error

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,3 +1,16 @@
+
+
 $(document).ready(function () {
-  $(".slider").owlCarousel();
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [980, 2],
+    itemsMobile: [600, 1],
+    nav: true,
+    navigation: false,
+    dots: true,
+    navText: ["prev", "next"],
+    pagination: true,
+    autoPlay: true,
+  });
 });
