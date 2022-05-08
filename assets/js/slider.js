19bed2bfa88b0b45294e6afccb7d4ee67d436363
najwanan <theunnamedrd@gmail.com>
2022-05-07 22:18:12 -0700

    trouble shooting jQuery Owl plugin, change order of scripts

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,16 +1,16 @@
 
 
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    items: 3,
-    itemsDesktop: [1199, 3],
-    itemsDesktopSmall: [980, 2],
-    itemsMobile: [600, 1],
-    nav: true,
-    navigation: false,
-    dots: true,
-    navText: ["prev", "next"],
-    pagination: true,
-    autoPlay: true,
-  });
+$(window).load(function () {
+    $(".owl-carousel").owlCarousel({
+      items: 3,
+      itemsDesktop: [1199, 3],
+      itemsDesktopSmall: [980, 2],
+      itemsMobile: [600, 1],
+      nav: true,
+      navigation: false,
+      dots: true,
+      navText: ["prev", "next"],
+      pagination: true,
+      autoPlay: true,
+    });
 });
